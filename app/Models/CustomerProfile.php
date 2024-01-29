1c4d51a2ababc0b243d437610bcef7ffefd8383b
Sanowarsunny <sanowarsunny1998@gmail.com>
2024-01-29 21:21:42 +0600

    model, table done

--- app/Models/CustomerProfile.php
+++ app/Models/CustomerProfile.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CustomerProfile extends Model
 {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-
+//pages
+Route::get('/', [HomeController::class, 'HomePage']);
