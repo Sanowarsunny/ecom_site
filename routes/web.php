764bcb343352cb26f5ce58a504b0e00dab63e5e6
Sanowarsunny <sanowarsunny1998@gmail.com>
2024-01-29 23:38:59 +0600

    route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //pages
 Route::get('/', [HomeController::class, 'HomePage']);
+Route::get('/by-category', [CategoryController::class, 'ByCategoryPage']);
+Route::get('/by-brand', [BrandController::class, 'ByBrandPage']);
+Route::get('/policy', [PolicyController::class, 'PolicyPage']);
+Route::get('/details', [ProductController::class, 'Details']);
+Route::get('/login', [UserController::class, 'LoginPage']);
+Route::get('/verify', [UserController::class, 'VerifyPage']);
+Route::get('/wish', [ProductController::class, 'WishList']);
+Route::get('/cart', [ProductController::class, 'CartListPage']);
+Route::get('/profile', [ProfileController::class, 'ProfilePage']);
+
+// Brand List
+Route::get('/BrandList', [BrandController::class, 'BrandList']);
+// Category List
+Route::get('/CategoryList', [CategoryController::class, 'CategoryList']);
+// Product List
+Route::get('/ListProductByCategory/{id}', [ProductController::class, 'ListProductByCategory']);
+Route::get('/ListProductByBrand/{id}', [ProductController::class, 'ListProductByBrand']);
+Route::get('/ListProductByRemark/{remark}', [ProductController::class, 'ListProductByRemark']);
+// Slider
+Route::get('/ListProductSlider', [ProductController::class, 'ListProductSlider']);
+// Product Details
+Route::get('/ProductDetailsById/{id}', [ProductController::class, 'ProductDetailsById']);
+Route::get('/ListReviewByProduct/{product_id}', [ProductController::class, 'ListReviewByProduct']);
+//policy
+Route::get("/PolicyByType/{type}",[PolicyController::class,'PolicyByType']);
+
+
+
+// User Auth
+Route::get('/UserLogin/{UserEmail}', [UserController::class, 'UserLogin']);
+Route::get('/VerifyLogin/{UserEmail}/{OTP}', [UserController::class, 'VerifyLogin']);
+Route::get('/logout',[UserController::class,'UserLogout']);
